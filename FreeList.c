9daef9e7c82c1f69afc113144c4bcc1c8253ea12
Bakewell Thomas (tcb2643) <tcb2643@CSR-A58120.csr.utexas.edu>
2024-05-31 15:37:05 -0500

    FreeList now compiles and headers are seemingly correctly implemented

--- FreeList.c
+++ FreeList.c
@@ -1,3 +1,7 @@
+/* Thomas Bakewell, University of Virginia
+ * Implementation of free list
+ * Based off OSTEP Chap 17
+*/
 #include <sys/mman.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,30 +24,34 @@ void* mallocc(size_t space, node_t* head){
 			
 			void* allocatedSpace = mmap(head,space+sizeof(header_t), 
 					PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
-			header_t* ptr = (header_t*) allocatedSpace -1;
+			header_t* ptr =  (&allocatedSpace) -1;
 			ptr->size = space+sizeof(node_t);
 			ptr->magic = random();
 			return allocatedSpace;
 		}
+		break;
 	}
 	return NULL;
 }
-
-
-
-
-
 
 int main(void){
 	node_t* head = mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);	head->size = 4096 - sizeof(node_t);
 	head->next = NULL;
 	
-	int* test = (int*) mallocc(5*sizeof(int), head);
+	int* test = (int*) mallocc(10*sizeof(int), head);
+	
+	if (test == NULL){
+		printf("Malloc Failed\n");
+		return -1;
+	}
+
 	int val = 5;
 	test = &val;
-
-	printf("Remaining Size = %d\n", head->size);
-
+	//printf("%d", (*test));
+	//printf("Remaining Size = %d\n", head->size);
+	
+	header_t* header = (header_t*) test -1;
+	//printf("%d\n", header->magic);
 	return 0;
 }
 
