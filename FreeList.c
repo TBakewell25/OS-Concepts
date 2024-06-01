78a2792c7bc0b8f86d8b541d73fe309d7a030b7c
Thomas <cbakewell1@gmail.com>
2024-06-01 06:34:51 +0000

    comments added

--- FreeList.c
+++ FreeList.c
@@ -7,25 +7,29 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+//struct for heap node
 typedef struct __node_t {
 	int size;
 	struct __node_t *next;
 } node_t;
 
+//struct for memory block headers
 typedef struct {
 	int size;
 	int magic;
 } header_t;
 
+//like 'malloc()', but worse
 void* mallocc(size_t space, node_t* head){
-	while (head->next == NULL){
+	while (head->next == NULL){ //TODO: add linked list logic
 		if (head->size >= space){
 			head->size = head->size - (space + sizeof(node_t));
 			
 			void* allocatedSpace = mmap(head,space+sizeof(header_t), 
 					PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
 			void* returnedMem = allocatedSpace+8;
-			header_t* ptr = allocatedSpace;
+			
+			header_t* ptr = allocatedSpace; //create header for new memory block
 			ptr->size = space;
 			ptr->magic = random();
 			return returnedMem;
