536b74c1116f952070221a6e4733d6dcdf0d4f8d
Thomas <cbakewell1@gmail.com>
2024-08-28 13:02:06 -0400

    added comments. working glibc queued locks

--- threads/utils/bit_test.h
+++ threads/utils/bit_test.h
@@ -2,22 +2,22 @@
 #define BIT_TEST_H
 
 int atomic_bit_test(int mutex, int bit){
-	int val = (mutex>>bit)&0x00000001;
-	if (val == 0){
+	int val = (mutex>>bit)&0x00000001; //grab leading bit, check it
+	if (val == 0){ 
 		return 0;
 	}
 	return 1;
 }
 
 void atomic_increment(int* mutex){
-	*mutex = *mutex | 0x80000000;
+	*mutex = *mutex | 0x80000000; //increment leading bit from 1 to 0
 	}
 
 void atomic_decrement(int* mutex){
-	*mutex = (*mutex << 1) >> 1;
+	*mutex = (*mutex << 1) >> 1; //decrement leading bit from 0 to 1
 	}
 
-int atomic_add_zero(int* mutex, int val){
+int atomic_add_zero(int* mutex, int val){ //0 out int
 	*mutex = *mutex + val;
 	return *mutex;
 	}	
--- threads/utils/mutex_lock.h
+++ threads/utils/mutex_lock.h
@@ -13,27 +13,29 @@ based on OSTEP
 
 void mutex_lock(int* mutex){
 	int k;
-	if (atomic_bit_test(*mutex, 31) == 0)
-		atomic_increment(mutex);
+	if (atomic_bit_test(*mutex, 31) == 0) //if the lock is unlocked
+		atomic_increment(mutex); //modified to lock the lock
 		return;
-	atomic_increment(mutex);
+	//atomic_increment(mutex); //otherwise increment the lock
+	//NOTE: exists in OSTEP implementation, moved to line 17, now it works
 	while(1){
-		if (atomic_bit_test(*mutex, 31) == 0){
-			atomic_decrement(mutex);
+		if (atomic_bit_test(*mutex, 31) == 0){ //test again if it is unlocked
+			atomic_increment(mutex); //same as above
+			//atomic_decrement(mutex); 
 			return;
 		}
 		k = *mutex;
 		if (k >= 0)
 			continue;
-		futex_wait(mutex, k);
+		futex_wait(mutex, k); //wait for lock queue
 
 	}
 }	
 
 void mutex_unlock(int* mutex){
-	if (atomic_add_zero(mutex, 0x80000000))
+	if (atomic_add_zero(mutex, 0x80000000)) //add 0x800..., 0 out 32 bit int
 		return;
-	futex_wake(mutex);
+	futex_wake(mutex); //wake waiting threads
 	}
 
 #endif
